7b3dc456ba7f5a77263f757c2aa37768fcc6db75
Yuriy Losev <yuriy.losev@flant.com>
2022-11-23 22:06:14 +0400

    ++
    Signed-off-by: Yuriy Losev <yuriy.losev@flant.com>

--- modules/015-admission-policy-engine/hooks/handle_operation_policies.go
+++ modules/015-admission-policy-engine/hooks/handle_operation_policies.go
@@ -1,6 +1,24 @@
+/*
+Copyright 2022 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package hooks
 
 import (
+	"k8s.io/utils/pointer"
+
 	v1alpha1 "github.com/deckhouse/deckhouse/modules/015-admission-policy-engine/hooks/internal/apis"
 
 	"github.com/flant/addon-operator/pkg/module_manager/go_hook"
@@ -10,13 +28,14 @@ import (
 )
 
 var _ = sdk.RegisterFunc(&go_hook.HookConfig{
-	OnBeforeHelm: &go_hook.OrderedConfig{Order: 30},
+	OnAfterHelm: &go_hook.OrderedConfig{Order: 30},
 	Kubernetes: []go_hook.KubernetesConfig{
 		{
-			Name:       "operation-policies",
-			ApiVersion: "deckhouse.io/v1alpha1",
-			Kind:       "OperationPolicy",
-			FilterFunc: filterOP,
+			Name:                         "operation-policies",
+			ApiVersion:                   "deckhouse.io/v1alpha1",
+			Kind:                         "OperationPolicy",
+			ExecuteHookOnSynchronization: pointer.Bool(false),
+			FilterFunc:                   filterOP,
 		},
 	},
 }, handleOP)
